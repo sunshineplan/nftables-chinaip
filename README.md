e520deed079375ba0c8780e50f197228ba182e9f
sunshineplan <sunshineplan@users.noreply.github.com>
2020-02-22 23:04:14 +0800

    Only export China IPv4

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Nftables geoip script
+# Nftables chinaip script
 
-Python script that generates .nft files with mappings betwween IP addresses
+Python script that generates .nft files with mappings between IP addresses
 and its geolocation, so you can include them inside your rules.
 
 # Requirements
@@ -15,7 +15,7 @@ To generate ipv4 and ipv6 mappings, download geoip data from db-ip.com
 saving the output in the current folder
 
 ```
-   ./nft_geoip.py --file-location location.csv --download
+   ./nft_geoip.py --download
 ```
 
 Giving the following output
@@ -24,49 +24,28 @@ Giving the following output
   drwxr-xr-x 2 foobar foobar 4,0K ene  4 19:38 .
   drwxr-xr-x 5 foobar foobar 4,0K ene  4 19:38 ..
   -rw-r--r-- 1 foobar foobar  22M ene  4 19:38 dbip.csv
-  -rw-r--r-- 1 foobar foobar  956 ene  4 19:38 geoip-def-africa.nft
-  -rw-r--r-- 1 foobar foobar 8,3K ene  4 19:38 geoip-def-all.nft
-  -rw-r--r-- 1 foobar foobar  902 ene  4 19:38 geoip-def-americas.nft
-  -rw-r--r-- 1 foobar foobar   15 ene  4 19:38 geoip-def-antarctica.nft
-  -rw-r--r-- 1 foobar foobar  808 ene  4 19:38 geoip-def-asia.nft
-  -rw-r--r-- 1 foobar foobar  810 ene  4 19:38 geoip-def-europe.nft
-  -rw-r--r-- 1 foobar foobar  461 ene  4 19:38 geoip-def-oceania.nft
-  -rw-r--r-- 1 foobar foobar 8,8M ene  4 19:38 geoip-ipv4.nft
-  -rw-r--r-- 1 foobar foobar  16M ene  4 19:38 geoip-ipv6.nft
+  -rw-r--r-- 1 foobar foobar 203K ene  4 19:38 chinaip-ipv4.nft
 ```
 
-* geoip-def-all defines variables for each country (2-char iso name)
-  in location.csv assigning its iso numeric value (eg. Canada, `define $CA = 124`)
-* geoip-ipv4.nft defines geoip4 map (@geoip4)
-* geoip-ipv6.nft defines geoip6 map (@geoip6)
+* chinaip-ipv4.nft defines chinaip4 map (@chinaip4)
+* China iso numeric value is 156
 
 ## Marking packets to its corresponding country
 
 ```
-  meta mark set ip saddr map @geoip4
+  meta mark set ip saddr map @chinaip4
 ```
 
-## Example: Counting incoming Spanish traffic
+## Example: Counting incoming China traffic
 
 ```
   table filter {
-    include "./geoip-def-all.nft"
-    include "./geoip-ipv4.nft"
-    include "./geoip-ipv6.nft"
+    include "./chinaip-ipv4.nft"
 
     chain input {
-                  type filter hook input priority filter; policy accept;
-                  meta mark set ip saddr map @geoip4
-                  meta mark $ES counter
+                  type filter hook input priority 0;
+                  meta mark set ip saddr map @chinaip4
+                  meta mark 156 counter
           }
   }
 ```
-
-# Caveats
-
-__It is not possible to use the country definitions inside an interactive
-nft shell.__
-
-So, although maps are defined, country definitions do not persist.
-You will need to define those geoip rules inside your ruleset definition
-files. Like in the example above.
--- nft_geoip.py
+++ nft_geoip.py
@@ -22,20 +22,6 @@ import time
 import unicodedata
 
 
-# entries in location csv
-GeoEntry = namedtuple('GeoEntry',
-                      'name, '
-                      'alpha_2, '
-                      'alpha_3, '
-                      'country_code, '
-                      'iso_3166_2, '
-                      'region, '
-                      'sub_region, '
-                      'intermediate_region, '
-                      'region_code, '
-                      'sub_region_code, '
-                      'intermediate_region_code')
-
 # entries in DB-IP geoip csv
 NetworkEntry = namedtuple('NetworkEntry',
                           'network_first, '
@@ -49,27 +35,6 @@ def strip_accent(text):
     """
     return ''.join(char for char in unicodedata.normalize('NFKD', text)
                    if unicodedata.category(char) != 'Mn')
-
-
-def make_country_and_continent_dict():
-    """
-    Returns three formatted dictionaries with following key/value:
-        - iso_3166_2 (numeric)  :   country name
-        - country_name          :   continent/region name
-        - country_name          :   alpha2 name
-    """
-    country_dict = {}
-    continent_dict = {}
-    country_alpha_dict = {}
-    next(args.locations)  # Omit license notice
-    next(args.locations)  # Omit csv header
-    for geo_entry in map(GeoEntry._make, csv.reader(args.locations)):
-        country_dict[geo_entry.country_code.lstrip('0')] = geo_entry.name
-        continent_dict[geo_entry.name] = geo_entry.region
-        country_alpha_dict[geo_entry.name] = geo_entry.alpha_2
-    return format_dict(country_dict), \
-           format_dict(continent_dict), \
-           format_dict(country_alpha_dict)
 
 
 def format_dict(dictionary):
@@ -91,50 +56,6 @@ def format_dict(dictionary):
     return new_dict
 
 
-def write_geoip_location(country_dict, continent_dict, country_alpha_dict):
-    """
-    Write country iso code definitions, separating files for each continent
-    (eg. geoip-def-asia.nft, etc.)
-    Also writes a definition file with all countries in "geoip-def-all.nft"
-    """
-    for continent in continent_dict.values():
-        with open(args.dir+'geoip-def-{}.nft'.format(continent), 'w') as output_file:
-            try:
-               for country, iso in [(country, iso)
-                                    for iso, country
-                                    in country_dict.items()
-                                    if continent_dict[country] == continent]:
-                    output_file.write('define {} = {}\n'.format(country_alpha_dict[country].upper(), iso))
-            except KeyError as e:
-                # 'ZZ' is used for an unknown country, so it won't match
-                # any country in the location file. Pass and do not write
-                # to output file
-                pass
-
-    with open(args.dir+'geoip-def-all.nft', 'w') as output_file:
-        for iso, country in country_dict.items():
-            output_file.write('define {} = {}\n'.format(country_alpha_dict[country].upper(), iso))
-        output_file.write('\n' * 2)
-
-        output_file.write('define africa = 1\n'
-                          'define asia = 2\n'
-                          'define europe = 3\n'
-                          'define americas = 4\n'
-                          'define oceania = 5\n'
-                          'define antarctica = 6\n')
-        output_file.write('\n')
-        output_file.write('map continent_code {\n'
-                          '\ttype mark : mark\n'
-                          '\tflags interval\n'
-                          '\telements = {\n\t\t')
-
-        output_file.write(',\n\t\t'.join(make_lines2({country_alpha_dict[country].upper():v
-                                                      for country, v
-                                                      in continent_dict.items()})))
-        output_file.write('\n')
-        output_file.write('\t}\n')
-        output_file.write('}\n')
-
 def check_ipv4(addr):
     """
     Returns true if a string is representing an ipv4 address.
@@ -147,29 +68,17 @@ def check_ipv4(addr):
         return False
 
 
-def make_geoip_dict(country_alpha_dict, ipv):
+def make_chinaip_dict():
     """
-    Read DB-IP network ranges and creates geoip4 and geoip6 dictionaries
-    mapping ip ranges over country alpha-2 codes
+    Read DB-IP network ranges and creates china ip4 dictionaries
     """
-    # XXX: country_alpha_dict is used to prune countries not appearing
-    #      inside location.csv (eg. Kosovo)
-    #
-    #      Kosovo could be added to location.csv with a "virtual" code
-    #      Kosovo,XK,XKX,999,ISO 3166-2:XK,Europe,"","","","",""
-    #
-    #      Or instead use geonames.
-    geoip_dict = {}
-
-    known_alphas = country_alpha_dict.values()
-
-    args.blocks.seek(0)
+    chinaip_dict = {}
 
     for net_entry in map(NetworkEntry._make, csv.reader(args.blocks)):
 
         alpha2 = net_entry.country_alpha_2
-        # 'ZZ' or codes not appearing in location.csv will be ignored
-        if (alpha2 == 'ZZ') or (alpha2.lower() not in known_alphas):
+        # codes not equal 'CN' will be ignored
+        if alpha2 != 'CN':
             continue
 
         # There are entries in DB-IP csv for single addresses which
@@ -180,30 +89,19 @@ def make_geoip_dict(country_alpha_dict, ipv):
         else:
             k = '-'.join((net_entry.network_first, net_entry.network_last))
 
-        if ipv == 'ipv4':
-            if check_ipv4(net_entry.network_first):
-                geoip_dict[k] = alpha2
-        else:
-            if not check_ipv4(net_entry.network_first):
-                geoip_dict[k] = alpha2
+        if check_ipv4(net_entry.network_first):
+                chinaip_dict[k] = alpha2
 
-    return format_dict(geoip_dict)
+    return format_dict(chinaip_dict)
 
 
-def make_lines1(dictionary):
+def make_lines(dictionary):
     """
     For each entry in the dictionary maps to a line for nftables dict files
     using key literal and value as nft variable.
     """
-    return ['{} : ${}'.format(k, v) for k, v in dictionary.items()]
+    return ['{} : 156'.format(k) for k, v in dictionary.items()]
 
-
-def make_lines2(dictionary):
-    """
-    For each entry in the dictionary maps to a line for nftables dict files
-    using key and value as nft variables.
-    """
-    return ['${} : ${}'.format(k, v) for k, v in dictionary.items()]
 
 def write_nft_header(f):
     """
@@ -213,42 +111,24 @@ def write_nft_header(f):
             .format(datetime.now().strftime("%a %b %d %H:%M %Y")))
     f.write("# IP Geolocation by DB-IP (https://db-ip.com) licensed under CC-BY-SA 4.0\n\n")
 
-def write_geoips(geoip4_dict, geoip6_dict):
+def write_chinaips(chinaip_dict):
     """
-    Write ipv4 and ipv6 geoip nftables maps to corresponding output files.
+    Write ipv4 chinaip nftables maps to corresponding output files.
     """
-    with open(args.dir+'geoip-ipv4.nft', 'w') as output_file:
+    with open(args.dir+'chinaip-ipv4.nft', 'w') as output_file:
         write_nft_header(output_file)
-        output_file.write('map geoip4 {\n'
+        output_file.write('map chinaip4 {\n'
                           '\ttype ipv4_addr : mark\n'
                           '\tflags interval\n'
                           '\telements = {\n\t\t')
 
-        output_file.write(',\n\t\t'.join(make_lines1({k:v.upper() for k,v in geoip4_dict.items()})))
+        output_file.write(',\n\t\t'.join(make_lines({k:v.upper() for k,v in chinaip_dict.items()})))
         output_file.write('\n')
         output_file.write('\t}\n')
         output_file.write('}\n')
-
-    with open(args.dir+'geoip-ipv6.nft', 'w') as output_file:
-        write_nft_header(output_file)
-        output_file.write('map geoip6 {\n'
-                          '\ttype ipv6_addr : mark\n'
-                          '\tflags interval\n'
-                          '\telements = {\n\t\t')
-
-        output_file.write(',\n\t\t'.join(make_lines1({k:v.upper() for k,v in geoip6_dict.items()})))
-        output_file.write('\n')
-        output_file.write('\t}\n')
-        output_file.write('}\n')
-
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Creates nftables geoip definitions and maps.')
-    parser.add_argument('--file-location',
-                        type=argparse.FileType('r'),
-                        help='path to csv file containing information about countries',
-                        required=True,
-                        dest='locations')
+    parser = argparse.ArgumentParser(description='Creates nftables chinaip definitions and maps.')
     parser.add_argument('--file-address',
                         type=argparse.FileType('r'),
                         help='path to db-ip.com lite cvs file with ipv4 and ipv6 geoip information',
@@ -294,21 +174,11 @@ if __name__ == '__main__':
         # Update blocks arg with the downloaded file
         args.blocks = open(args.dir+'dbip.csv', 'r', encoding='utf-8')
 
-    if not (args.blocks or args.locations):
-        parser.print_help()
-        sys.exit('Missing required address and location csv files.')
     if not args.blocks:
         parser.print_help()
         sys.exit('Missing geoip address csv file. You can instead download it using --download.')
-    if not args.locations:
-        parser.print_help()
-        sys.exit('Missing country information csv file')
 
-    country_dict, continent_dict, country_alpha_dict = make_country_and_continent_dict()
-    print('Writing country definition files...')
-    write_geoip_location(country_dict, continent_dict, country_alpha_dict)
-    print('Writing nftables maps (geoip-ipv{4,6}.nft)...')
-    geoip4_dict = make_geoip_dict(country_alpha_dict, 'ipv4')
-    geoip6_dict = make_geoip_dict(country_alpha_dict, 'ipv6')
-    write_geoips(geoip4_dict, geoip6_dict)
+    print('Writing nftables maps chinaip-ipv4.nft...')
+    chinaip_dict = make_chinaip_dict()
+    write_chinaips(chinaip_dict)
     print('Done!')
